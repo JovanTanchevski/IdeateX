854a74aa2c26ec62dc776e10b0b6219d4dc37542
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-08-04 16:10:58 +0200

    Imported the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import './App.css';
 import { AuthContext } from './context/AuthContext';
 import React, { useContext } from 'react';
+import Header from './components/Header/Header';
 function App() {
   const { isLogged } = useContext(AuthContext);
-  console.log(isLogged);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-3xl font-bold underline">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
