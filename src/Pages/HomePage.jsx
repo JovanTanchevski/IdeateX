de98a33a50039160aa7a24e735109146a140a9fc
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-08-21 13:31:30 +0200

    Added Pages folder with the Homepage function component

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BannerSection from '../components/HomePage/BannerSection';
+import '../components/HomePage/Styles/HomePage.css';
+const HomePage = () => {
+  return (
+    <React.Fragment>
+      <BannerSection />
+    </React.Fragment>
+  );
+};
+
+export default HomePage;
