12c9824304ade96d838457edb8a1fe4218f27221
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-09-11 15:58:02 +0200

    Enlargened the text to 6xl

--- src/components/HomePage/BannerSection.jsx
+++ src/components/HomePage/BannerSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const BannerSection = () => {
   return (
     <div className="items-center flex flex-col justify-center py-32 banner-section ">
-      <h1 className="text-4xl font-semibold text-center">
+      <h1 className="text-6xl font-semibold text-center">
         Unleash Your Innovative Potential
       </h1>
       <h2 className="lg:text-xl mt-10">
