139724e909a33fe7d31920e05e76af78666b9241
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-09-11 17:07:29 +0200

    Added the footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import SingleEventPage from './Pages/Single-Event-Page/SingleEventPage';
 import Error404Page from './components/404Page/Error404Page';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<p>Login Page</p>} />
         <Route path="*" element={<Error404Page />} />
       </Routes>
+      <Footer />
     </React.Fragment>
   );
 }
