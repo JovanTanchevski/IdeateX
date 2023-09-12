54c1aefaf36e70d9d537b7bf4f4548e65dd4038e
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-09-12 14:25:35 +0200

    Dynamically implementing the login form showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import SingleEventPage from './Pages/Single-Event-Page/SingleEventPage';
 import Error404Page from './components/404Page/Error404Page';
 import Footer from './components/Footer/Footer';
+import LoginPage from './components/LoginPage/LoginPage';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+  const { isLogged } = useContext(AuthContext);
   return (
     <React.Fragment>
       <Header />
@@ -14,7 +17,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/event/:eventID" element={<SingleEventPage />} />
         <Route path="/about" element={<p>About</p>} />
-        <Route path="/login" element={<p>Login Page</p>} />
+        {!isLogged && <Route path="/login" element={<LoginPage />} />}
         <Route path="*" element={<Error404Page />} />
       </Routes>
       <Footer />
