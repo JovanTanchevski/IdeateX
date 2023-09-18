c663338e251fe0df07028c2acf415bb70bca424e
Jovan Tanchevski <101511857+JovanTanchevski@users.noreply.github.com>
2023-09-18 13:34:31 +0200

    Login Page Updates

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,43 +1,29 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLogged, setIsLogged] = useState(false);
-  const [adminUsers, setAdminUsers] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const storedIsLogged = localStorage.getItem('isLogged');
     if (storedIsLogged) {
       setIsLogged(JSON.parse(storedIsLogged));
     }
-    fetch('http://localhost:4000/admin_users')
-      .then((res) => res.json())
-      .then((data) => setAdminUsers(data));
   }, []);
 
-  const onLoginHandleClick = (inputUsername, inputPassword) => {
-    const matchedUser = adminUsers.find(
-      ({ username, password }) =>
-        username === inputUsername && password === inputPassword
-    );
-
-    if (matchedUser) {
+  const onLoginHandleClick = (username, password) => {
+    // In a real app, you would perform authentication here
+    // For this example, let's just assume a simple check
+    if (username === 'user' && password === 'password') {
       setIsLogged(true);
       localStorage.setItem('isLogged', JSON.stringify(true));
-      navigate('/');
-      return true; // Login successful
     }
-
-    return false; // Login failed
   };
 
   const onLogOutHandleClick = () => {
     setIsLogged(false);
     localStorage.removeItem('isLogged');
-    navigate('/login');
   };
 
   return (
